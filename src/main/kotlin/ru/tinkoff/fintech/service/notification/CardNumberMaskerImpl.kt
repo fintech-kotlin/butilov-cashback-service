24c78e88e1ae1828732f53b86f81c07f704288df
Dmitry Butilov <d.butilov@aamsystems.ru>
2020-02-24 08:40:15 +0300

    add number masker implementation

--- src/main/kotlin/ru/tinkoff/fintech/service/notification/CardNumberMaskerImpl.kt
+++ src/main/kotlin/ru/tinkoff/fintech/service/notification/CardNumberMaskerImpl.kt
@@ -3,6 +3,8 @@ package ru.tinkoff.fintech.service.notification
 class CardNumberMaskerImpl: CardNumberMasker {
 
     override fun mask(cardNumber: String, maskChar: Char, start: Int, end: Int): String {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        if (start > end) throw Exception("Start index cannot be greater than end index")
+        val chars = cardNumber.mapIndexed { i, char -> if (i in start until end) maskChar else char }
+        return String(chars.toCharArray())
     }
 }
