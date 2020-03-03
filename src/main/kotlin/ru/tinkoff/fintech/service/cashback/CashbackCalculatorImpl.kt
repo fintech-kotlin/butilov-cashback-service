cf45c197c5512f2e0c6b6f362152e3e36d33b1ea
Dmitry Butilov <d.butilov@aamsystems.ru>
2020-03-03 17:57:33 +0300

    add impl of proposals on PR #2

--- src/main/kotlin/ru/tinkoff/fintech/service/cashback/CashbackCalculatorImpl.kt
+++ src/main/kotlin/ru/tinkoff/fintech/service/cashback/CashbackCalculatorImpl.kt
@@ -4,7 +4,6 @@ import ru.tinkoff.fintech.model.TransactionInfo
 import java.math.BigDecimal
 import java.math.RoundingMode
 import java.time.LocalDateTime.now
-import java.time.Month
 import java.time.Month.*
 
 internal const val LOYALTY_PROGRAM_BLACK = "BLACK"
@@ -35,9 +34,11 @@ class CashbackCalculatorImpl : CashbackCalculator {
 
     override fun calculateCashback(transactionInfo: TransactionInfo): Double {
         val possibleCashback = MAX_CASH_BACK - transactionInfo.cashbackTotalValue
-        return if (possibleCashback == 0.0) 0.0 else {
+        return if (possibleCashback != 0.0) {
             val cashback = calculate(transactionInfo)
-            return if (possibleCashback <= cashback) possibleCashback else cashback
+            if (possibleCashback <= cashback) possibleCashback else cashback
+        } else {
+            0.0
         }
     }
 
@@ -46,46 +47,38 @@ class CashbackCalculatorImpl : CashbackCalculator {
         return round(cashback)
     }
 
-    private fun getAward666(info: TransactionInfo): Double {
-        val sum = info.transactionSum
-        return if (sum == 666.0 || sum % 666.0 == 0.0) 6.66 else 0.0
+    private fun getAward666(info: TransactionInfo) = if (info.transactionSum % 666.0 == 0.0) 6.66 else 0.0
+
+    private fun getLoyaltyPart(info: TransactionInfo): Double = when (info.loyaltyProgramName) {
+        LOYALTY_PROGRAM_BLACK -> 0.01
+        LOYALTY_PROGRAM_ALL -> getAllLoyaltyPart(info)
+        LOYALTY_PROGRAM_BEER -> getBeerLoyaltyPart(info)
+        else -> 0.0
     }
 
-    private fun getLoyaltyPart(info: TransactionInfo): Double {
-        return when (info.loyaltyProgramName) {
-            LOYALTY_PROGRAM_BLACK -> 0.01
-            LOYALTY_PROGRAM_ALL -> getAllLoyaltyPart(info)
-            LOYALTY_PROGRAM_BEER -> getBeerLoyaltyPart(info)
-            else -> 0.0
+    private fun getAllLoyaltyPart(info: TransactionInfo) = with(info) {
+        if (mccCode == MCC_SOFTWARE && isCustomPalindrome(transactionSum)) {
+            (nok(firstName.length, lastName.length) / 100000.0)
+        } else {
+            0.0
         }
     }
 
-    private fun getAllLoyaltyPart(info: TransactionInfo): Double {
-        with(info) {
-            return if (mccCode == MCC_SOFTWARE && isCustomPalindrome(transactionSum)) {
-                (nok(firstName.length, lastName.length) / 100000.0)
-            } else {
-                0.0
+    private fun getBeerLoyaltyPart(info: TransactionInfo): Double = with(info) {
+        if (mccCode == MCC_BEER) {
+            val firstName = info.firstName.toLowerCase()
+            val lastName = info.lastName.toLowerCase()
+            val currentMonth = now().month
+            when {
+                firstName == "олег" && lastName == "олегов" -> 0.10
+                firstName == "олег" -> 0.07
+                firstName[0] == monthValueToFirstRusLetterMap[currentMonth.value] -> 0.05
+                firstName[0] == monthValueToFirstRusLetterMap[currentMonth.minus(1).value] -> 0.03
+                firstName[0] == monthValueToFirstRusLetterMap[currentMonth.plus(1).value] -> 0.03
+                else -> 0.02
             }
-        }
-    }
-
-    private fun getBeerLoyaltyPart(info: TransactionInfo): Double {
-        with(info) {
-            return if (mccCode == MCC_BEER) {
-                if (firstName.toLowerCase() == "олег") {
-                    if (lastName.toLowerCase() == "олегов") 0.10 else 0.07
-                } else {
-                    val current = now().month
-                    val nameChar = getFirstNameFirstLetter()
-                    if (getMonthFirstChar(current) == nameChar) return 0.05 else {
-                        val previous = current.minus(1)
-                        val next = current.plus(1)
-                        val condition = getMonthFirstChar(previous) == nameChar || getMonthFirstChar(next) == nameChar
-                        if (condition) 0.03 else 0.02
-                    }
-                }
-            } else 0.0
+        } else {
+            0.0
         }
     }
 
@@ -93,20 +86,20 @@ class CashbackCalculatorImpl : CashbackCalculator {
         return BigDecimal(cashback).setScale(2, RoundingMode.HALF_EVEN).toDouble()
     }
 
-    private fun TransactionInfo.getFirstNameFirstLetter() = firstName[0].toLowerCase()
-
     private fun isCustomPalindrome(transactionSum: Double): Boolean {
         val sum = transactionSum * 100
-        if (sum == 0.0) {
-            return true
+        return if (sum == 0.0) {
+            true
         } else {
             val array = sum.toInt().toString().toCharArray()
-            return customCheck(array)
+            customCheck(array)
         }
     }
 
     private tailrec fun customCheck(array: CharArray, count: Int = 0, i: Int = 0, n: Int = array.size): Boolean {
-        return if (count == 2) false else {
+        return if (count == 2) {
+            false
+        } else {
             if (i > n shr 1) {
                 count < 2
             } else {
@@ -123,10 +116,6 @@ class CashbackCalculatorImpl : CashbackCalculator {
     private tailrec fun nod(val1: Int, val2: Int): Int {
         val mod = val1 % val2
         return if (mod <= 0) val2 else nod(val2, mod)
-    }
-
-    private fun getMonthFirstChar(month: Month): Char {
-        return (monthValueToFirstRusLetterMap[month.value] ?: error("Month not found"))
     }
 
 }
