ca3da5fd1d66e6b63b987b522a322225e658e655
Dmitry Butilov <d.butilov@aamsystems.ru>
2020-03-23 22:12:48 +0300

    add repository method with acync run

--- src/main/kotlin/ru/tinkoff/fintech/db/repository/LoyaltyPaymentRepository.kt
+++ src/main/kotlin/ru/tinkoff/fintech/db/repository/LoyaltyPaymentRepository.kt
@@ -1,8 +1,21 @@
 package ru.tinkoff.fintech.db.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 import ru.tinkoff.fintech.db.entity.LoyaltyPaymentEntity
+import java.time.LocalDateTime
 
 @Repository
-interface LoyaltyPaymentRepository : JpaRepository<LoyaltyPaymentEntity, Long>
+interface LoyaltyPaymentRepository : JpaRepository<LoyaltyPaymentEntity, Long> {
+
+     suspend fun findAllByCardIdAndSignAndDateTimeAfter(
+          cardId: String,
+          sign: String,
+          dateTime: LocalDateTime
+     ): Set<LoyaltyPaymentEntity>
+
+//     @Query("SELECT SUM(value) FROM LoyaltyPaymentEntity " +
+//             "WHERE cardId = ?1 AND sign = ?2 AND dateTime > ?3 ")
+//     suspend fun findPossibleCashback(cardId: String, sign: String, currentMonth: LocalDateTime): Double
+}
--- src/main/kotlin/ru/tinkoff/fintech/service/transaction/TransactionServiceImpl.kt
+++ src/main/kotlin/ru/tinkoff/fintech/service/transaction/TransactionServiceImpl.kt
@@ -12,8 +12,8 @@ import ru.tinkoff.fintech.db.repository.LoyaltyPaymentRepository
 import ru.tinkoff.fintech.model.*
 import ru.tinkoff.fintech.service.cashback.CashbackCalculator
 import ru.tinkoff.fintech.service.notification.NotificationService
+import java.time.LocalDate
 import java.time.LocalDateTime
-import java.time.LocalDateTime.now
 
 @Service
 class TransactionServiceImpl(
@@ -27,20 +27,28 @@ class TransactionServiceImpl(
 
     override fun handle(transaction: Transaction) = runBlocking {
         val card = cardServiceClient.getCard(transaction.cardNumber)
-        val clientTask = async {
-            clientService.getClient(card.client)
-        }
 
+        val selectAllProgramsFromDbTask = async { findPossibleCashback(card) }
+        val getClientHttpCallTask = async { clientService.getClient(card.client) }
         val loyaltyProgram = loyaltyServiceClient.getLoyaltyProgram(card.loyaltyProgram)
 
-        val client = clientTask.await()
-        val cashback = calculateCashBack(loyaltyProgram, transaction, client)
+        val client = getClientHttpCallTask.await()
+        val totalCashback = selectAllProgramsFromDbTask.await()
+        val cashback = calculateCashback(loyaltyProgram, totalCashback, client, transaction)
 
         val transactionData = TransactionData(transaction, card, cashback)
         launch {
             saveLoyaltyPayment(transactionData)
         }
         sendNotification(transactionData, loyaltyProgram, client)
+    }
+
+    private suspend fun findPossibleCashback(card: Card): Double {
+        val currentMonthStartTime = LocalDate.now().withDayOfMonth(1).atStartOfDay()
+        return loyaltyPaymentRepository
+            .findAllByCardIdAndSignAndDateTimeAfter(card.id, sign, currentMonthStartTime)
+            .map { it.value }
+            .sum()
     }
 
     private fun sendNotification(transactionData: TransactionData, loyaltyProgram: LoyaltyProgram, client: Client) {
@@ -63,11 +71,16 @@ class TransactionServiceImpl(
         }
     }
 
-    private fun calculateCashBack(loyaltyProgram: LoyaltyProgram, transaction: Transaction, client: Client): Double {
+    private fun calculateCashback(
+        program: LoyaltyProgram,
+        totalCashback: Double,
+        client: Client,
+        transaction: Transaction
+    ): Double {
         TransactionInfo(
-            loyaltyProgram.name,
+            program.name,
             transaction.value,
-            3000.0, // todo где взять?
+            totalCashback,
             transaction.mccCode,
             client.birthDate!!.toLocalDate(),
             client.firstName!!,
@@ -79,13 +92,13 @@ class TransactionServiceImpl(
     }
 
     companion object {
-        private const val sign = "not compile time const"
+        private const val sign = "i'am not a compile-time constant"
 
         private data class TransactionData(
             val transaction: Transaction,
             val card: Card,
             val cashback: Double,
-            val time: LocalDateTime = now()
+            val time: LocalDateTime = LocalDateTime.now()
         )
     }
 }
